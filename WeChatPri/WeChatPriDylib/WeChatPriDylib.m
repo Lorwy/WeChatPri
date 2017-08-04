5ab95fd40c1254dfb7cdc2fdfb74cac502db1345
Lorwy <598308224@qq.com>
2017-08-04 14:07:56 +0800

    隐藏发现页面的摇一摇、附近的人、购物入口

--- WeChatPri/WeChatPriDylib/WeChatPriDylib.m
+++ WeChatPri/WeChatPriDylib/WeChatPriDylib.m
@@ -14,7 +14,36 @@
 #import <Cycript/Cycript.h>
 #import <CydiaSubstrate/CydiaSubstrate.h>
 
-static __attribute__((constructor)) void entry(){
+
+
+//-fno-objc-arc
+
+//CHDeclareClass(UIApplication)
+//CHDeclareClass(MicroMessengerAppDelegate)
+//CHDeclareClass(CMessageMgr)
+CHDeclareClass(FindFriendEntryViewController)
+//CHDeclareClass(MMTabBarController)
+//CHDeclareClass(MMBadgeView)
+//CHDeclareClass(WCDeviceStepObject)
+//CHDeclareClass(NewMainFrameViewController)
+//CHDeclareClass(UIView)
+//CHDeclareClass(NewSettingViewController)
+//CHDeclareClass(MMTableViewInfo)
+//CHDeclareClass(MMTableViewSectionInfo)
+//CHDeclareClass(MMTableViewCellInfo)
+//CHDeclareClass(MMTableView)
+//CHDeclareClass(UIViewController)
+//CHDeclareClass(UILabel)
+//CHDeclareClass(ChatRoomInfoViewController)
+
+@interface FindFriendEntryViewController:UIViewController
+
+- (void)reloadData;
+
+@end
+
+static __attribute__((constructor)) void entry()
+{
     NSLog(@"\n               🎉!!！congratulations!!！🎉\n👍----------------insert dylib success----------------👍");
     
     [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
@@ -23,26 +52,46 @@ static __attribute__((constructor)) void entry(){
     }];
 }
 
-CHDeclareClass(CustomViewController)
 
-CHOptimizedMethod(0, self, NSString*, CustomViewController,getMyName){
-    //get origin value
-    NSString* originName = CHSuper(0, CustomViewController, getMyName);
-    
-    NSLog(@"origin name is:%@",originName);
-    
-    //get property
-    NSString* password = CHIvar(self,_password,__strong NSString*);
-    
-    NSLog(@"password is %@",password);
-    
-    //change the value
-    return @"AloneMonkey";
-    
+//MARK: 清理发现页面
+CHOptimizedMethod2(self, CGFloat, FindFriendEntryViewController, tableView, UITableView *, tableView, heightForRowAtIndexPath, NSIndexPath *, indexPath)
+{
+    if ((indexPath.section == 1 && indexPath.row == 1)
+        || (indexPath.section == 2 && indexPath.row == 0)
+        || (indexPath.section == 3 && indexPath.row == 0))
+    {
+        return 0;
+    }
+    return CHSuper2(FindFriendEntryViewController, tableView, tableView, heightForRowAtIndexPath, indexPath);
 }
 
+CHOptimizedMethod2(self, UITableViewCell *, FindFriendEntryViewController, tableView, UITableView *, tableView, cellForRowAtIndexPath, NSIndexPath *, indexPath)
+{
+    UITableViewCell *cell = CHSuper2(FindFriendEntryViewController, tableView, tableView, cellForRowAtIndexPath, indexPath);
+    if ((indexPath.section == 1 && indexPath.row == 1)
+        || (indexPath.section == 2 && indexPath.row == 0)
+        || (indexPath.section == 3 && indexPath.row == 0))
+    {
+        NSLog(@"## 隐藏摇一摇、附近的人、购物 ##");
+        cell.hidden = YES;
+        for (UIView *subview in cell.subviews) {
+            [subview removeFromSuperview];
+        }
+    }
+    return cell;
+}
+
+CHOptimizedMethod1(self, void, FindFriendEntryViewController, viewDidAppear, BOOL, animated)
+{
+    CHSuper1(FindFriendEntryViewController, viewDidAppear, animated);
+    [self performSelector:@selector(reloadData)];
+}
+
+
 CHConstructor{
-    CHLoadLateClass(CustomViewController);
-    CHClassHook(0, CustomViewController, getMyName);
+    CHLoadLateClass(FindFriendEntryViewController);
+    CHHook2(FindFriendEntryViewController, tableView, heightForRowAtIndexPath);
+    CHHook2(FindFriendEntryViewController, tableView, cellForRowAtIndexPath);
+    CHHook1(FindFriendEntryViewController, viewDidAppear);
 }
 
